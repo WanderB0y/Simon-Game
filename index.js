ea4906989a9c5f7e8722474ca51e564db3b7a62a
Chupaeng <villaralvorovic2@gmail.com>
2023-12-04 08:39:04 +0800

    Add generators

--- index.js
+++ index.js
@@ -2,30 +2,38 @@ const greenButton = document.querySelector('#green');
 const redButton = document.querySelector('#red');
 const yellowButton = document.querySelector('#yellow');
 const blueButton = document.querySelector('#blue');
-
+const levelTitle = document.querySelector('#level-title');
+const bodyColor = document.body;
+ 
 let arrayNumColours = [];
 let arrayNumColoursClone = [];
 let level = arrayNumColours.length;
 let goPlay = false;
-let countLevel = 0;
+let countLevel = 1;
 
+document.addEventListener('keypress', function(e){
+    if(e.key === 'a'){
+        goPlay = true;
+    }else{
+        errorInput();
+    }
+})
 
+function errorInput () {
+    const audio = new Audio('./sounds/wrong.mp3');
+    audio.muted = false;
+    bodyColor.classList.add('red');
 
+    setTimeout(() => {
+        bodyColor.classList.remove('red')
+        audio.play();
+    }, 200)
+}
 
 greenButton.addEventListener('click', (e) => {
     animateClassListAndPlayAudio(greenButton, greenButton.getAttribute('id'));
 
-    if (goPlay){
-        arrayNumColoursClone.push(1);
-
-        for(let i = 0; i < arrayNumColours.length; i++){
-            if(arrayNumColours[i] === arrayNumColoursClone[i]){
-                continue;
-            } else{
-
-            }
-        }
-    }
+    goPlayTheGame();
 });
 
 redButton.addEventListener('click', (e) => {
@@ -41,34 +49,7 @@ blueButton.addEventListener('click', (e) => {
     animateClassListAndPlayAudio(blueButton, blueButton.getAttribute('id'));
 });
 
-function animateClassListAndPlayAudio(buttonColour, buttonColourID){
-    //Play Audio
-    let colourAudio = `./sounds/${buttonColourID}.mp3`
-    const audio = new Audio(colourAudio);
-    audio.muted = false;
-    audio.play();
-
-    //AnimatedButton
-    buttonColour.classList.add('pressed');
-    setTimeout(() => {
-        buttonColour.classList.remove('pressed')
-    }, 200)
-}
-
-
-function randomColourNumberGenerator() {
-    
-    return Math.floor(Math.random() * 4);
-}
-
-function resetCurrentStats (){
-    arrayNumColours.splice(0, arrayNumColours.length);
-    countLevel = 0;
-}
-
-
-
-
+// Will simulate the click
 function simulateClick(i, level, arrayNumColours) {
     if (i < level) {
         switch(arrayNumColours[i]){
@@ -91,7 +72,38 @@ function simulateClick(i, level, arrayNumColours) {
     }
 }
 
+// This function will play the aiuddio an will animate particulr class
+function animateClassListAndPlayAudio(buttonColour, buttonColourID){
+    //Play Audio
+    let colourAudio = `./sounds/${buttonColourID}.mp3`
+    const audio = new Audio(colourAudio);
+    audio.muted = false;
+    audio.play();
+
+    //AnimatedButton
+    buttonColour.classList.add('pressed');
+    setTimeout(() => {
+        buttonColour.classList.remove('pressed')
+    }, 200)
+}
 
 
-simulateClick(0, arrayNumColours.length, arrayNumColours)
-console.log(randomColourNumberGenerator())
+const generateRandNumber = function* () {
+    yield Math.floor(Math.random() * 4)
+
+}
+
+function resetCurrentStats (){
+    errorInput();
+    goPlay = false;
+    arrayNumColours.splice(0, arrayNumColours.length);
+    arrayNumColoursClone.splice(0, arrayNumColoursClone.length);
+    countLevel = 0;
+}
+
+//Check if the game is playable
+
+
+for(let i = 0; i < countLevel; i++){
+    console.log(generateRandNumber().next().value);
+}
